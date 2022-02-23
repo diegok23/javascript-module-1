a3dd86323a034446bff1197cfe200d4fef2f5f71
diegok23 <dkogut@gmail.com>
2022-02-23 20:10:08 +0100

    Hecho en clase

---
+++ week-1/InClass/exercise-D.js
@@ -0,0 +1,5 @@
+let colors = "blue, yellow";
+
+const messageType = typeof colors;
+
+console.log(messageType);
---
+++ week-1/InClass/exercise-E.js
@@ -0,0 +1,8 @@
+const saludo = "Hola, mi nombre es";
+const nombre = "Diego";
+
+const opcionConcatenar = saludo + " " + nombre;
+const opcionInterpolar = `${saludo} ${nombre}`;
+
+console.log("Ejercicio concatenando", opcionConcatenar);
+console.log("Ejercicio interpolando", opcionInterpolar);
