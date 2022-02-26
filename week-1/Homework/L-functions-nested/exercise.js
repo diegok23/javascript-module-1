070e7313c22fcba0b20bd2e3d7bd4da32664edb3
diegok23 <dkogut@gmail.com>
2022-02-26 12:34:00 +0100

    Ejercicio completado

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,19 @@
+function mentorName(name) {
+  return name.toUpperCase();
+}
+
+function greetingMentor(name, greeting) {
+  return `${greeting} ${mentorName(name)}`;
+}
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+console.log(greetingMentor(mentor1, "HELLO"));
+console.log(greetingMentor(mentor2, "HELLO"));
+console.log(greetingMentor(mentor3, "HELLO"));
+console.log(greetingMentor(mentor4, "HELLO"));
+console.log(greetingMentor(mentor5, "HELLO"));
