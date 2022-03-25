8a2a30b62abcbfc8f9dbcc8a2a7438680d4532d6
diegok23 <dkogut@gmail.com>
2022-03-25 14:51:30 +0100

    Corregido

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,4 @@
 const userName = " Daniel  ";
-const message = `My name is ${userName.trim()} and my name is ${userName.length} characters long`;
+const userNameCleaned = userName.trim();
+const message = `My name is ${userNameCleaned} and my name is ${userNameCleaned.length} characters long`;
 console.log(message);
