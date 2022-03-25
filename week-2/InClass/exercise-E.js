ce0080d4775ce1152e718df7ebb61814b077b71a
diegok23 <dkogut@gmail.com>
2022-03-25 15:23:14 +0100

    Corregido

--- week-2/InClass/exercise-E.js
+++ week-2/InClass/exercise-E.js
@@ -1,22 +1,21 @@
 let num = 10;
 
-num > 5 && num < 15;
-true;
+console.log(num > 5 && num < 15);
+/* true */
 
-num < 10 || num === 10;
-true;
+console.log(num < 10 || num === 10);
+/* true */
 
-false || true;
-true;
+console.log(false || true);
+/* true */
 
-!true;
-false;
+console.log(!true);
+/* false */
 
 let greaterThan5 = num > 5;
-true;
 
-!greaterThan5;
-false;
+console.log(!greaterThan5);
+/* false */
 
-!(num === 10);
-false;
+console.log(!(num === 10));
+/* false */
