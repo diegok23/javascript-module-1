54cfe32d7a2b540a5a8f9de15b994d0e37936af7
diegok23 <dkogut@gmail.com>
2022-03-25 14:41:21 +0100

    Ejecicios hechos

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
 
+const userName = 'Diego'
+const message = `My name is ${userName} and my name is ${userName.length} characters long`
 console.log(message);
--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,3 @@
-const name = " Daniel  ";
-
+const userName = " Daniel  ";
+const message = `My name is ${userName.trim()} and my name is ${userName.length} characters long`;
 console.log(message);
